a45b685f2a840a9052d91706fa78052d87993ee7
Wenguang Xu <Wenguang_Xu@gnwc.ca>
2014-11-04 23:36:37 -0800

    ui imported

---
+++ UnityProject/Client/Assets/Code/UI/ControlBtn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlBtn : MonoBehaviour 
+{
+	[HideInInspector]
+	public static string CurrentKeyCode;
+	public string keyCode;
+
+	public void BtnClicked()
+	{
+		CurrentKeyCode = keyCode;
+	}
+
+}
