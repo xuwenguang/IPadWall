28475ee8265cd4f2d2c09a1166a54f2ae131673d
cjlzgc <nick_zhao@mbp-c8-41.local>
2014-11-05 15:56:53 -0800

    add videos test on computer SUCCESS!

--- UnityProject/Client/Assets/Scripts/VideoControl/VideoSetup.cs
+++ UnityProject/Client/Assets/Scripts/VideoControl/VideoSetup.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class VideoSetup : MonoBehaviour {
 
 	public	static	VideoSetup	Instance;
+	public	static	int			groupID;
+
 	[SerializeField]
 	private Transform		_camera;
 	[SerializeField]
-	private	GameObject[]	_videoPlane;
+	private	Transform		_videoPlane;
 	[SerializeField]
-	private	GameObject		_centreVideoPlane;
+	private	Vector3[]		_videoSizes;
 
 	void Awake () {
 		Instance = this;
@@ -17,41 +19,37 @@ public class VideoSetup : MonoBehaviour {
 
 	public void LocateCamera (string name) {
 		string[] strs = name.Split('_');
-		int groupID = int.Parse(strs[0]);
+		groupID = int.Parse(strs[0]);
 		int cameraIndex = int.Parse(strs[1]);
-		if(groupID != 5) {
-			if(groupID == 0 || groupID == 1) {
-				SetupGroup12(groupID, cameraIndex);
-			} else if(groupID == 2 || groupID == 3) {
-				SetupGroup34(groupID, cameraIndex);
-			}
 
+		if(groupID == 0 || groupID == 1) {
+			SetupGroup12(cameraIndex);
+		} else if(groupID == 2 || groupID == 3) {
+			SetupGroup34(cameraIndex);
 		} else {
-			_centreVideoPlane.SetActive(true);
-			VideoTexture videoTextureScript = _centreVideoPlane.GetComponent<VideoTexture>();
-			videoTextureScript.jumpToVideo(0);
+			PlayVideo(0);
 		}
 
 	}
 
-	void SetupGroup12 (int groupID, int cameraIndex) {
+	void SetupGroup12 (int cameraIndex) {
 		//setup camera
-		_camera.position = new Vector3(((cameraIndex % 5) - 3) * (8f / 3f), -(cameraIndex / 5 * 2 - 1)  * 1.5f, -10);
+		_camera.position = new Vector3(((cameraIndex % 5) - 2) * 4, -(cameraIndex / 5 * 2 - 1)  * 1.5f, -10);
 		//play video
-		PlayVideo(groupID, 0);
+		PlayVideo(0);
 	}
 	
-	void SetupGroup34 (int groupID, int cameraIndex) {
+	void SetupGroup34 (int cameraIndex) {
 		//setup camera
 		_camera.position = new Vector3(((cameraIndex % 2) * 2 - 1) * 2, -(cameraIndex / 2 - 1)  * 3f, -10);
 		//play video
-		PlayVideo(groupID, 0);
+		PlayVideo(0);
 	}
 
-	public void PlayVideo (int groupID, int videoIndex) {
+	public void PlayVideo (int videoIndex) {
 		//play video
-		_videoPlane[groupID / 2].SetActive(true);
-		VideoTexture videoTextureScript = _videoPlane[groupID / 2].GetComponent<VideoTexture>();
+		_videoPlane.localScale = _videoSizes[groupID / 2];
+		VideoTexture videoTextureScript = _videoPlane.GetComponent<VideoTexture>();
 		videoTextureScript.jumpToVideo(videoIndex * 5 + groupID);
 	}
 
