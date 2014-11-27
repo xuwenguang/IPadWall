5949d3a1c43e2dd25464947fa0140528388860a4
Wenguang Xu <Wenguang_Xu@gnwc.ca>
2014-11-26 23:50:14 -0800

    timeKeeper

---
+++ UnityProject/Controller/Assets/code/UI/TimeKeeper.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeKeeper : MonoBehaviour {
+	
+	public delegate void TimeCallBack();
+	[HideInInspector]
+	public float lastClickTime;
+	
+	void Start()
+	{
+		lastClickTime = Time.time;
+		InvokeRepeating ("timeInvok",0f,5f);
+	}
+	
+	private void _reSetTime()
+	{
+		lastClickTime = Time.time;
+	}
+
+	void Update()
+	{
+		if (Input.GetButton("Fire1"))
+		{
+			_reSetTime();
+		}
+	}
+
+
+	public void timeInvok()
+	{
+		if(Time.time-lastClickTime>=5)
+		{
+			Debug.Log("more than 45 seconds not clicked");
+
+		}
+	}
+	
+	
+}
